ab7d2e5820f8acd510c2046c630920a8758549dc
Dr.Linux <88365653+DrLinuxOfficial@users.noreply.github.com>
2021-12-19 13:22:41 -0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name="reCaptchaBypasser",
+version="1.0",
+description="This Package For Bypassing Any reCaptcha For Selenium Python .",
+long_description = "README.md",
+long_description_content_type = "text/markdown",
+url="https://github.com/DrLinuxOfficial/reCaptchaBypasser-Py",
+author="Dr.Linux",
+packages=["reCaptchaBypasser"])
